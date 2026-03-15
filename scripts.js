39365e947c157ea40084835c3abd94c3ce44645f
melisamadula <melisamadula.mm@gmail.com>
2026-03-15 18:18:27 +0200

    Made modal small screen responsive and added background clicking

--- scripts.js
+++ scripts.js
@@ -30,3 +30,9 @@ closeBtn.addEventListener("click", function () {
   modal.close();
 });
   
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
